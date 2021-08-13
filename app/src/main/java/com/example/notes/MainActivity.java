89b7a99aaef66e548e828ce7efd5ee8ce394e268
Masharo <naronwaz0gmail.com>
2021-08-13 15:56:47 +0500

    добавлено акстивити для создания задач

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.notes;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
@@ -48,5 +50,10 @@ public class MainActivity extends AppCompatActivity {
 
         NotesAdapter adapter = new NotesAdapter(this, notes);
         recyclerViewNotes.setAdapter(adapter);
+    }
+
+    public void onClickAddNote(View view) {
+        Intent intent = new Intent(this, AddNoteActivity.class);
+        startActivity(intent);
     }
 }
